bccaff22e99bc19ae4e8c03fcb440ed11e57ed18
jerry <668Jerry@gmail.com>
2014-06-14 09:04:20 +0800

    debug create

--- article/views.py
+++ article/views.py
@@ -8,6 +8,14 @@ import datetime
 from article.models import Article
 from django.shortcuts import render
 
+from django import forms
+from django.http import HttpResponseRedirect
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'content', ]
+
 def home(request):
     s = "This is the Main Page."
     return HttpResponse(s)
@@ -37,20 +45,13 @@ def detail(request, pk):
     article = Article.objects.get(pk=int(pk))
     return render(request, "detail.html", {'article': article})
 
-from django import forms
-
-class ArticleForm(forms.ModelForm):
-    class Meta:
-        model = Article
-        fields = ['title', 'content', ]
-
-from django.http import HttpResponseRedirect
-
 def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         if form.is_valid():
             new_article = form.save()
             return HttpResponseRedirect('/article/' + str(new_article.pk))
+        else:
+            return HttpResponseRedirect('/article/1')
     form = ArticleForm()
     return render(request, 'create_article.html', {'form': form})
